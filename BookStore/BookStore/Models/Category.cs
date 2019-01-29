2632854a7afbc1685dbbbc2f9c6ab4febf52cd17
LeontievVlad <vladyslaw.leontyew@gmail.com>
2019-01-29 12:10:08 +0200

    AddCategoryClass

---
+++ BookStore/BookStore/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<Article> Articles { get; set; }
+    }
+
+    public class Article
+    {
+
+    }
+}
