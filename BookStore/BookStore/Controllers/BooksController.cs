c5962380d579f589bf9cc136fbe5fc5b7078fe46
LeontievVlad <46972096+LeontievVlad@users.noreply.github.com>
2019-03-13 01:42:45 +0200

    Update BooksController.cs

--- BookStore/BookStore/Controllers/BooksController.cs
+++ BookStore/BookStore/Controllers/BooksController.cs
@@ -19,25 +19,7 @@ namespace BookStore.Controllers
         private string strCom = "Com";
 
         
-    // GET: Books
-    //public ActionResult Index()
-    //{
-    //    var books = db.Books.Include(b => b.Category);
-    //    return View(books.ToList());
-    //}
-
-    ////Get: Your Information
-    //public string YourInfo()
-    //{
-    //    string browser = HttpContext.Request.Browser.Browser;
-    //    string user_agent = HttpContext.Request.UserAgent;
-    //    string url = HttpContext.Request.RawUrl;
-    //    string ip = HttpContext.Request.UserHostAddress;
-    //    string referrer = HttpContext.Request.UrlReferrer == null ? "" : HttpContext.Request.UrlReferrer.AbsoluteUri;
-    //    return "<p>Browser: " + browser + "</p><p>User-Agent: " + user_agent + "</p><p>Url запроса: " + url +
-    //        "</p><p>Реферер: " + referrer + "</p><p>IP-адрес: " + ip + "</p>";
-    //}
-
+    
     // Get: View
     [HttpGet]
         public ActionResult View(string search, int? page)
@@ -78,7 +60,7 @@ namespace BookStore.Controllers
         {
             ViewBag.catname = new SelectList(db.Categories, "CategoryId", "NameCategory");
             ViewBag.authorname = new SelectList(db.Books, "BookId", "Author");
-            //ViewBag.ratBook = new SelectList(db.Books, "BookId", "RatingBook");
+            
 
             int pageSize = 3;
             int pageNumber = (page ?? 1);
@@ -99,13 +81,7 @@ namespace BookStore.Controllers
                 books = db.Books.OrderByDescending(x => x.RatingBook).ToPagedList(pageNumber, pageSize);
                 return View(books);
             }else
-            //if (ratBook > 0)
-            //{
-
-            //    books = db.Books.Where(x => x.BookId == ratBook).OrderBy(x => x.BookId).ToPagedList(pageNumber, pageSize);
-            //    return View(books);
-
-            //}
+            
             if (authorname > 0)
             {
 
@@ -128,13 +104,6 @@ namespace BookStore.Controllers
         }
 
 
-        //OrderByDescending();
-
-
-        
-
-
-
 
         // GET: Books/Details/5
         public ActionResult Details(int? id)
@@ -148,7 +117,7 @@ namespace BookStore.Controllers
             var count = db.Comments.Where(b => b.ThisBook == id).Count();
             var ratingCom = db.Comments.Where(b => b.ThisBook == id).Sum(x => x.Rating);
             var rating = ratingCom / count;
-            //ViewBag.rat = rating;
+            
 
             book.RatingBook = rating;
             db.Entry(book).State = EntityState.Modified;
@@ -209,96 +178,13 @@ namespace BookStore.Controllers
             db.SaveChanges();
 
 
-            //var comment = db.Comments.Where(c => c.ThisBook == Bookid).ToList();
-            //2. Save to list
-
-            //List<CommentList> comments = new List<CommentList>
-            //    {
-            //        new CommentList(db.Comments.Find(Bookid))
-            //    };
-            //Session[strCom] = comments;
+           
 
             return RedirectToAction("Details", new { id = Bookid });
-            //return PartialView("AddComment", new Comment());
-            //return View(comment);
+            
         }
 
-        //// GET: Books/Create
-        //public ActionResult Create()
-        //{
-        //    ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "NameCategory");
-        //    return View();
-        //}
-
-        //// POST: Books/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "BookId,Title,Author,Desription,Price,ImagePath,ImageFile,CategoryId")] Book book)
-        //{
-        //    string filename = Path.GetFileNameWithoutExtension(book.ImageFile.FileName);
-        //    string extension = Path.GetExtension(book.ImageFile.FileName);
-        //    filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
-        //    book.ImagePath = "~/images/" + filename;
-        //    filename = Path.Combine(Server.MapPath("~/images/"), filename);
-        //    book.ImageFile.SaveAs(filename);
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Books.Add(book);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "NameCategory", book.CategoryId);
-        //    return View(book);
-        //}
-
-        //// GET: Books/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Book book = db.Books.Find(id);
-        //    if (book == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "NameCategory", book.CategoryId);
-        //    return View(book);
-        //}
-
-        //// POST: Books/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "BookId,Title,Author,Desription,Price,ImagePath,ImageFile,CategoryId")] Book book)
-        //{
-        //    string filename = Path.GetFileNameWithoutExtension(book.ImageFile.FileName);
-        //    string extension = Path.GetExtension(book.ImageFile.FileName);
-        //    filename = filename + DateTime.Now.ToString("yymmssfff") + extension;
-        //    book.ImagePath = "~/images/" + filename;
-        //    filename = Path.Combine(Server.MapPath("~/images/"), filename);
-
-        //    book.ImageFile.SaveAs(filename);
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(book).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "NameCategory", book.CategoryId);
-        //    return View(book);
-        //}
-
-        //// GET: Books/Delete/5
         
-        //// POST: Books/Delete/5
         
 
         protected override void Dispose(bool disposing)
